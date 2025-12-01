aa86f433e698287d5173c59adebcaec40cf241bd
net818 <165484543+net818@users.noreply.github.com>
2025-12-01 14:54:03 +0800

    Update msgparse.py

--- msgparse.py
+++ msgparse.py
@@ -1,4 +1,3 @@
-
 def thread_message(thread, ai_description):
     msg = (
         f"{thread['domain'].upper()} 新促销\n"
@@ -6,9 +5,7 @@ def thread_message(thread, ai_description):
         f"作者：{thread['creator']}\n"
         f"时间：{thread['pub_date'].strftime('%Y/%m/%d %H:%M')}\n\n"
         + (
-            f"{ai_description[:200].strip()}"
-            + ("..." if len(ai_description) > 200 else "")
-            + "\n\n"
+            f"{ai_description.strip()}\n\n"
             if ai_description else ""
         )
         + f"{thread['link']}"
@@ -20,14 +17,11 @@ def comment_message(thread, comment, ai_description):
         f"{thread['domain'].upper()} 新评论\n"
         f"作者：{comment['author']}\n"
         f"时间：{comment['created_at'].strftime('%Y/%m/%d %H:%M')}\n\n"
-        f"{comment['message'][:200].strip()}...\n\n"
+        f"{comment['message'].strip()}\n\n"
         + (
-            f"{ai_description[:200].strip()}"
-            + ("..." if len(ai_description) > 200 else "")
-            + "\n\n"
+            f"{ai_description.strip()}\n\n"
             if ai_description else ""
         )
         + f"{comment['url']}"
     )
     return msg
-
